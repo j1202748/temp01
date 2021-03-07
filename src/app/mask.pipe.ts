3ee5e7277e86f24578308ceb97144fb783aec865
Jamie <j1202748@gmail.com>
2021-03-07 16:50:06 +0800

    字定義Pipe練習 mask

---
+++ src/app/mask.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mask'
+})
+export class MaskPipe implements PipeTransform {
+
+  transform(value: string): string {
+
+    if(value && value.length >3)
+    {
+      return value.substring(0,3) +'****';
+    }
+    else
+    {
+      return '****';
+    }
+  }
+
+}
